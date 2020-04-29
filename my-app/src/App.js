fd506c523fe053ea019338e14018b4c6a4e5fb01
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-28 22:01:45 -0600

    Linked Dashboard to login page. Still need to figure out how to run authentication. Began to style dashboard. Implemented search bar. Next will be to hook up api data to search function and populate rendered playlist.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-// import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
-// import Login from './Login.js';
+import Login from './Login.js';
 import SignUp from './SignUp.js';
-// import Dashboard from './Dashboard.js';
+import Dashboard from './Dashboard.js';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      {/* <Dashboard /> */}
-        {/* <Login /> */}
-        <SignUp />
+      <Route path='/dashboard' component={Dashboard} />
+      <Route path='/login' component={Login} />
+      <Route path='/signup' component={SignUp} />
     </div>
+    </Router>
   );
 }
 
--- my-app/src/Dashboard.js
+++ my-app/src/Dashboard.js
@@ -1,5 +1,9 @@
-import React, {useState} from 'react';
+import React, {Component, PropTypes} from 'react';
+import Search from 'react-search'
+import {BrowserRouter as Router, Switch, Route, Link,} from 'react-router-dom';
+import Login from './Login.js';
 import './Dashboard.css';
+import SearchBar from './SearchBar.js';
 
 
 
@@ -7,23 +11,30 @@ const Dashboard = (props) => {
 
 
 
+
+
     return(
+        <div>
         <div className='dashboard'>
             <header>
             <nav>
                 <ul>
-                    <li>log out</li>
-                    <li>|</li>
-                    <li>new search</li>
+                    <Link className='link' to='/login'>log out</Link>
                 </ul>
             </nav>
             <div className='header-text'>
+            <h2 className='left-side'>Hey, there!</h2>
+            <h2 className='left-side-btm'>Let's get started.</h2>
             <h1 className='song'>Song</h1>
             <h1 className='suggester'>Suggester</h1>
             </div>
             </header>
+            <SearchBar />
         </div>
+        </div>
+
     )
 }
+
 
 export default Dashboard
